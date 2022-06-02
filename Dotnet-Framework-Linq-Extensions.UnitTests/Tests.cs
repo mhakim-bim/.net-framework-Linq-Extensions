2791cf9d08033f2319c27820afd603a02efdebcd
Mohamed Elhakim <melhakim2016@gmail.com>
2022-06-02 12:42:01 +0200

    initial

---
+++ Dotnet-Framework-Linq-Extensions.UnitTests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Dotnet_Framework_Linq_Extensions.UnitTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
