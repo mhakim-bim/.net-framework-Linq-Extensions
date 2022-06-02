dc579611dcb8f3cd436a842b40fdb1b0f1888688
Mohamed Elhakim <melhakim2016@gmail.com>
2022-06-02 13:05:18 +0200

    added whereIf extenions

---
+++ Dotnet-Framework-Linq-Extensions.UnitTests/WhereTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Dotnet_Framework_Linq_Extensions.UnitTests
+{
+    [TestFixture]
+    public class WhereTests
+    {
+        [Test]
+        public void WhereIf_ConditionIsTrue_ExpectedOutput()
+        {
+            var list = new List<string>()
+            {
+                "Mohamed","Ahmed","Mahmoud","Ali"
+            };
+            
+            var result = list.WhereIf(true,name => name[0].Equals('M')).ToList();
+            
+            Assert.True(result.Count == 2);
+        }
+        
+        [Test]
+        public void WhereIf_ConditionIsFalse_ExpectedOutput()
+        {
+            var list = new List<string>()
+            {
+                "Mohamed","Ahmed","Mahmoud","Ali"
+            };
+            
+            var result = list.WhereIf(false,name => name[0].Equals('M')).ToList();
+            
+            Assert.True(result.Count == 4);
+        }
+        
+    }
+}
---
+++ Dotnet-Framework-Linq-Extensions/WhereExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dotnet_Framework_Linq_Extensions
+{
+    public static class WhereExtensions
+    {
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> iqQueryable,bool condition,Func<T,bool> predicate)
+        {
+            if(!condition)
+                return iqQueryable;
+
+            return iqQueryable.Where(predicate).AsQueryable();
+        }
+        
+        public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> iEnumerable,bool condition,Func<T,bool> predicate)
+        {
+            if(!condition)
+                return iEnumerable;
+
+            return iEnumerable.Where(predicate);
+        }
+        
+       
+        
+    }
+}
